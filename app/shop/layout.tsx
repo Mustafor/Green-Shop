670fcc79ee1ac2df50bcec0295ce9f5ae8bb2bb8
Mustafor <nazokatabduvohidova83@gmail.com>
2025-01-24 20:42:45 +0500

    Layout

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,16 +1,19 @@
-"use client"
-import { usePathname } from 'next/navigation'
-import React, { ReactNode } from 'react'
+"use client";
+import { usePathname } from "next/navigation";
+import React, { ReactNode } from "react";
 
-const layout:React.FC<{children:ReactNode}> = ({children}) => {
-    const path = usePathname()
+const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const path = usePathname();
 
   return (
     <div>
-        <div className='px-[120px] text-[15px]'><span className='text-black font-bold'>Home</span> / Shop {path.includes("shopping-cart") && "/ Shopping Cart"}</div>
-        {children}
+      <div className="px-[120px] text-[15px]">
+        <span className="text-black font-bold">Home</span> / Shop
+        {path.includes("shopping-cart") && "/ Shopping Cart"}
+      </div>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default Layout;
